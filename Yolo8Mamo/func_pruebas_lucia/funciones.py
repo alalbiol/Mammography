1aa7c0d34c211eaa7291ccbe2d35f5903a35de93
LuciaLR <lloprib@upv.edu.es>
2024-10-03 13:42:08 +0200

    pruebas hechas sobre ddsm y cbis_ddsm

--- Yolo8Mamo/func_pruebas_lucia/funciones.py
+++ Yolo8Mamo/func_pruebas_lucia/funciones.py
@@ -1,28 +1,11 @@
-import numpy as np
+#Bucle para agrupar todos los casos de una carpeta en una variable
+
 import os
-import globc
-import subprocess
+# Itera sobre los archivos en el directorio
+for nombre_archivo in os.listdir(directorio):
+    # Verifica si es un archivo (no una carpeta)
+    if os.path.isfile(os.path.join(directorio, nombre_archivo)):
+        archivos.append(nombre_archivo)
 
-
-def read_lossless_dcm_raw(fname):
-    """Read raw image data from lossless dcm."""
-    #use the standford cmdline tool to decompress
-    try:
-        mess=subprocess.check_output(['pvrg-dcm','-d','-s',fname],
-                                stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as e:
-        # it raises the No trailing marker found! error,
-        # but it still does the job
-        # should check for error
-        mess=e.output
-        pass
-    
-    #read uncompressed image
-    fname = str(fname)
-    with open(fname+'.1', 'rb') as infile:
-        data= np.fromfile(infile, dtype='>u2')
-    
-    #remove the tmp image file made by the converter
-    subprocess.call(['rm',fname+'.1'])
-    
-    return data
+# Imprime la lista de archivos
+print(archivos)
