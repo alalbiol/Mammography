6ea72cc4bf7ac2d2ffb94cba7a22f404c36d95f8
LuciaLR <lloprib@upv.edu.es>
2024-10-03 09:20:54 +0200

    funciones

---
+++ Yolo8Mamo/pruebas_lucia/funciones.py
@@ -0,0 +1,22 @@
+def read_lossless_dcm_raw(fname):
+    """Read raw image data from lossless dcm."""
+    #use the standford cmdline tool to decompress
+    try:
+        mess=subprocess.check_output(['pvrg-dcm','-d','-s',fname],
+                                stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        # it raises the No trailing marker found! error,
+        # but it still does the job
+        # should check for error
+        mess=e.output
+        pass
+    
+    #read uncompressed image
+    fname = str(fname)
+    with open(fname+'.1', 'rb') as infile:
+        data= np.fromfile(infile, dtype='>u2')
+    
+    #remove the tmp image file made by the converter
+    subprocess.call(['rm',fname+'.1'])
+    
+    return data
