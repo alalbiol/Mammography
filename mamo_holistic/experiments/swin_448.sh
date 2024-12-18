48ea620118d1fd8a56dfe4dadd92ce8fff7df001
Alberto Albiol <alalbiol@iteam.upv.es>
2024-12-18 14:34:12 +0100

    configuraciones swin

--- mamo_holistic/experiments/swin_448.sh
+++ mamo_holistic/experiments/swin_448.sh
@@ -57,8 +57,8 @@ echo "Current working directory: $(pwd)"
 # python train/train.py --config config_files/swin/swin_224.yaml    \
 #              --overrides  config_files/swin/swin_224_variants/swin_large_patch4_window7_224.ms_in22k.yaml
 
-# python train/train.py --config config_files/swin/swin_224.yaml    \
-#             --overrides  config_files/swin/swin_224_variants/swin_large_patch4_window7_224.ms_in22k_lora.yaml
+python train/train.py --config config_files/swin/swin_x2_448.yaml     
+            #--overrides  config_files/swin/swin_224_variants/swin_large_patch4_window7_224.ms_in22k_lora.yaml
 
-python train/train.py --config config_files/swin/swin_224.yaml  
+
 
