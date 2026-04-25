332231a5fa52a16c23f169ed353ff67e76ad548f
Elsa <eblanov@teleco.upv.es>
2026-04-25 09:04:00 +0200

    Exp E01_BS_150

--- elsa/experiments/resnet_224.sh
+++ elsa/experiments/resnet_224.sh
@@ -20,14 +20,14 @@ cd ..
 echo "Current working directory: $(pwd)"
 
 # 1. ESCRIBE AQUÍ TU COMENTARIO PARA WANDB
-export WANDB_NOTES="Exp E01: ¿Batch optimo? Probando BS=75. ResNet50, parches 224x224, Num_Epochs=50."
+export WANDB_NOTES="Exp E01: ¿Batch optimo? Probando BS=150. ResNet50, parches 224x224, Num_Epochs=50."
 
 # 2. LANZA EL EXPERIMENTO
 NUM_EPOCHS=50 python train/train.py \
     --config_file config_files/base_config.yaml \
     --overrides config_files/resnet/resnet_50.yaml \
-    --batch_size 75 \
-    --job_name "E01_BS_75"
+    --batch_size 150 \
+    --job_name "E01_BS_150"
 
 #run experiments for resnet18, resnet34, resnet50 with 224x224 patches
 #enable Mammo conda environment
