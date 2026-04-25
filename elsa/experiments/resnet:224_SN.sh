ee516e5a85ae950ac05f3113e9c31091ff5c3628
Elsa <eblanov@teleco.upv.es>
2026-04-25 10:45:55 +0200

    Exp E02_SN

---
+++ elsa/experiments/resnet:224_SN.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# This script is used to run the experiments to see resnet18, resnet34, resnet50 with 224x224 patches.
+
+# Initialize Conda
+if [ -d "$HOME/miniconda3" ]; then
+    source $HOME/miniconda3/etc/profile.d/conda.sh
+elif [ -d "$HOME/anaconda3" ]; then
+    source $HOME/anaconda3/etc/profile.d/conda.sh
+else
+    echo "Conda not found in the default directories"
+fi
+
+# Activate the Mammo environment
+conda activate proymam
+echo "proymam environment activated"
+
+
+cd ..
+echo "Current working directory: $(pwd)"
+
+# 1. ESCRIBE AQUÍ TU COMENTARIO PARA WANDB
+export WANDB_NOTES="Exp E02: ¿Añadimos imágenes normales Probando SN. ResNet50, parches 224x224, Num_Epochs=50."
+
+
+python train/train.py --config config_files/base_config.yaml --overrides config_files/resnet/resnet_50.yaml
