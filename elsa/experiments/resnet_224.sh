e0158776e70f457c948b8134f909c7b679d81bed
Elsa <eblanov@teleco.upv.es>
2026-04-28 19:20:34 +0200

    Exp E01_BS100_2

---
+++ elsa/experiments/resnet_224.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# This script is used to run the experiments to see resnet18, resnet34, resnet50 with 224x224 patches.
+
+# Initialize Conda
+if [ -d "$HOME/miniconda3" ]; then
+    source $HOME/miniconda3/etc/profile.d/conda.sh
+elif [ -d "$HOME/anaconda3" ]; then
+    source $HOME/anaconda3/etc/profile.d/conda.sh
+else
+    echo "Conda not found in the default directories"
+fi
+
+# Activate the Mammo environment
+conda activate proymam
+echo "proymam environment activated"
+
+
+cd ..
+echo "Current working directory: $(pwd)"
+
+# 1. ESCRIBE AQUÍ TU COMENTARIO PARA WANDB
+export WANDB_NOTES="Exp E01: ¿Batch optimo? Probando BS=100. ResNet50, parches 224x224, Num_Epochs=150."
+
+# 2. LANZA EL EXPERIMENTO
+NUM_EPOCHS=250 python train/train.py \
+    --config_file config_files/base_config.yaml \
+    --overrides config_files/resnet/resnet_50.yaml \
+    --batch_size 100 \
+    --job_name "E01_BS_100"
